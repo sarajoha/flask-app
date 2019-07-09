5a20190033380ec9c0f178cdd54c714a3fa1701b
sarajoha <sjcc333@gmail.com>
2019-07-08 19:06:57 -0500

    Dividing app into modules

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from app import create_app
+from app.forms import LoginForm
 
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
-
-bootstrap = Bootstrap(app)
 
 todos = ['Terminar modulo de curso de Flask', 'Hacer planning de Julio', 'TODO 3']
 
+app = create_app()
 
 @app.cli.command()
 def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner().run(tests)
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 
 
 @app.errorhandler(404)
