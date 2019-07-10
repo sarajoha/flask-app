6dceb696c7ff24b0bfe1b44c9bc638130e8b2d8f
sarajoha <sjcc333@gmail.com>
2019-07-10 17:40:24 -0500

    Added delete and uptadte for todos

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import request, make_response, redirect, render_template, session, ur
 from flask_login import login_required, current_user
 import unittest
 from app import create_app
-from app.firestore_service import get_users, get_todos
+from app.firestore_service import get_todos, put_todo, delete_todo, update_todo
+from app.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
 
 
 app = create_app()
@@ -31,16 +32,45 @@ def index():
     return response
 
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET', 'POST'])
 @login_required
 def hello():
     user_ip = session.get('user_ip')
     username = current_user.id
+    todo_form = TodoForm()
+    delete_todo_form = DeleteTodoForm()
+    update_todo_form = UpdateTodoForm()
 
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username),
-        'username': username
+        'username': username,
+        'todo_form': todo_form,
+        'delete_todo_form': delete_todo_form,
+        'update_todo_form': update_todo_form,
     }
 
+    if todo_form.validate_on_submit():
+        put_todo(username, todo_form.description.data)
+
+        flash('Tu tarea ha sido creada con éxito')
+
+        return redirect(url_for('hello'))
+
     return render_template('hello.html', **context)
+
+
+@app.route('/todos/delete/<todo_id>', methods=['POST'])
+def delete(todo_id):
+    user_id = current_user.id
+    delete_todo(user_id, todo_id)
+
+    return redirect(url_for('hello'))
+
+
+@app.route('/todos/update/<todo_id>/<int:done>', methods=['POST'])
+def update(todo_id, done):
+    user_id = current_user.id
+    update_todo(user_id, todo_id, done)
+
+    return redirect(url_for('hello'))
